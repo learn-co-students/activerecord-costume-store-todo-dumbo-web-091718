06d422aeba4c44c86800d5a3e63ee8755118479f
github username <github email address>
2018-09-29 16:46:17 -0400

    addded 3 tables: costumes, store, haunted_houses

--- app/models/costume_store.rb
+++ app/models/costume_store.rb
@@ -1,1 +1,12 @@
-# Create your CostumeStore class here
+class Create_costumes < ActiveRecord::Base
+
+  def change
+    create_table :costumes do |t|
+      t.string :name
+      t.integer :price
+      t.integer :size
+      t.sting :image_url
+    end
+  end
+
+end
