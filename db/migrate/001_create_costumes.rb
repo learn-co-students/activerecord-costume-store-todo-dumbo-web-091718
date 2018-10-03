d24f099d283ff455525a068ee35e16e02088d74f
milanla <milan.lanlada@gmail.com>
2018-10-03 08:46:23 -0400

    Done

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -15,6 +15,7 @@ class CreateCostumes < ActiveRecord::Migration[5.1]
       t.string :size
       t.string :image_url
       t.datetime :created_at
+      t.datetime :updated_at
     end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -16,7 +16,7 @@ ActiveRecord::Schema.define(version: 3) do
     t.string "name"
     t.string "location"
     t.integer "costume_inventory"
-    t.integer "num_of_employee"
+    t.integer "num_of_employees"
     t.boolean "still_in_business"
     t.datetime "opening_time"
     t.datetime "closing_time"
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 3) do
     t.string "size"
     t.string "image_url"
     t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "haunted_houses", force: :cascade do |t|
